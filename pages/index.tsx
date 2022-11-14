4a2bb7d6c0b0c1cbe4bb8007364a594d02084b54
ujlm <maesulysse@gmail.com>
2022-11-14 12:48:01 +0100

    noreferrer

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,24 +56,7 @@ type Photo = {
     username: string;
     name: string;
   };
-};
-
-const PhotoComp: React.FC<{ photo: Photo }> = ({ photo }) => {
-  const { user, urls } = photo;
-
-  return (
-    <Fragment>
-      <img className="img" src={urls.regular} />
-      <a
-        className="credit"
-        target="_blank"
-        href={`https://unsplash.com/@${user.username}`}
-      >
-        {user.name}
-      </a>
-    </Fragment>
-  );
-};
+}
 
 const Home: NextPage = () => {
 
